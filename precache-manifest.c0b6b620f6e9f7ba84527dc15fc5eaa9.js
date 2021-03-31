0bf2e1bdfdcb9dc9fb15dbbb565f2d53246177cb
Agustin Lleras <lleras.ag@gmail.com>
2021-03-30 21:39:47 -0400

    Updates

---
+++ precache-manifest.c0b6b620f6e9f7ba84527dc15fc5eaa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71c328aeb9bd851297efa2be8254913",
+    "url": "/covid-dashboard/index.html"
+  },
+  {
+    "revision": "2b942962c29e31b77633",
+    "url": "/covid-dashboard/static/css/main.3f9dd97c.chunk.css"
+  },
+  {
+    "revision": "ae37f599a7a8b0c63607",
+    "url": "/covid-dashboard/static/js/2.5ab05974.chunk.js"
+  },
+  {
+    "revision": "8f1725caf6c94099afb359796db79c4f",
+    "url": "/covid-dashboard/static/js/2.5ab05974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b942962c29e31b77633",
+    "url": "/covid-dashboard/static/js/main.35d92cac.chunk.js"
+  },
+  {
+    "revision": "71f385a77d97a31025fd",
+    "url": "/covid-dashboard/static/js/runtime-main.0feba0cb.js"
+  },
+  {
+    "revision": "b927f9e0848128b7748748fcf1a49cb6",
+    "url": "/covid-dashboard/static/media/coffee.b927f9e0.bin"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid/precache-manifest.f53fe6e3d3be105f1415049d997ed396.js"
+  "/covid-dashboard/precache-manifest.c0b6b620f6e9f7ba84527dc15fc5eaa9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
