373bec51e4c93f98da090d73588ce3ca82afbd46
Agustin Lleras <lleras.ag@gmail.com>
2021-03-30 21:19:56 -0400

    Positivity rate and 7 day average calculations

--- src/Util/dataCalculations.js
+++ src/Util/dataCalculations.js
@@ -1,15 +1,27 @@
-var getPositivityRate = (dailyTests, dailyNewCases) => {
+import * as d3 from 'd3';
+
+function getPositivityRate(dailyTests, dailyNewCases) {
     let totalTests = 0;
     let totalNewCases = 0;
 
-    for(let i=0; i<dailyNewCases.length;i++){
-        if(dailyTests[i] < 0 || dailyNewCases[i] < 0) continue;
-        console.log(dailyTests[i], dailyNewCases[i])
+    for (let i = 0; i < dailyNewCases.length; i++) {
+        if (dailyTests[i] <= 0 || dailyNewCases[i] <= 0)
+            continue;
         totalTests += dailyTests[i];
         totalNewCases += dailyNewCases[i];
     }
-    console.log(totalTests, totalNewCases)
+
     return (totalNewCases / totalTests) * 100;
 }
+/*
+    Gets the seven day average of a specified key in an array of objects.
+*/
+function get7DayAverageKey(data, index, key) {
+    let newData = data.slice(index - 3, index + 3)
+        .map(el => el[key]);
 
-export {getPositivityRate}
+    let mean = d3.mean(newData);
+    return mean === undefined ? 0 : mean;
+}
+
+export {getPositivityRate, get7DayAverageKey}
